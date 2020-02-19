b2609a52404018e135285490eb3daa6a06bd875a
CesarPS15 <cesarpreciado42@gmail.com>
2020-02-19 16:49:35 -0600

    Prueba

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('prueba/{nombre?}', function($nombre = 'desconocido'){
+  $nombre = strtoupper($nombre);
+  return view('vista-prueba')->with(['nombre'=> $nombre]);
+});
